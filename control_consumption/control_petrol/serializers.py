9a95f0a6cbffbfd583d120b5b1ff90edf22b4f4e
Yroslav326 <yaroslav-kotov-91@mail.ru>
2025-05-02 22:04:54 +0300

    Создание модели и API

---
+++ control_consumption/control_petrol/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from control_consumption.control_petrol import models
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','username', 'email')
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Station
+        fields = ('id','station_name','region','distance')
+
+class RouteSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = models.Route
+        fields = ('id','person','station','car','date')
---
+++ control_consumption/control_petrol/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('stations/', views.StationList.as_view()),
+    path('stations/<int:pk>/', views.StationDetail.as_view()),
+    path('routes/', views.RouteList.as_view()),
+    path('routes/<int:pk>/', views.RouteDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
